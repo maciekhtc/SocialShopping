af2f402810a5928a08b97e0963ee004682f316a6
Maciej Sieradzki <maciej.janusz.sieradzki@gmail.com>
2016-05-24 21:42:32 +0200

    Added administrator account hardcoded, for future management

--- Account/Login.aspx.cs
+++ Account/Login.aspx.cs
@@ -15,7 +15,13 @@ public partial class Account_Login : Page
     protected void Button1_Click(object sender, EventArgs e)        //logowanie
     {
         DataView dv = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
-        if (dv.Table.Rows.Count!=0)
+        if (TextBox1.Text.Equals("administrator") && TextBox2.Text.Equals("adminpassword"))    //admin?
+        {
+            Session["User_Id"] = -99 + "";
+            Session["User_Username"] = "administrator";
+            Page.Response.Redirect("~/Default.aspx");
+        }
+        else if (dv.Table.Rows.Count != 0)
         {
             Session["User_Id"] = (Int64)dv.Table.Rows[0][0]+"";
             Session["User_Username"] = (string)dv.Table.Rows[0][1];
